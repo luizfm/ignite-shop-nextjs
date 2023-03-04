0a588ced9438e2ae5b3db8d0cf5df31cf5297a5a
Luiz Fernando De Morais <luizfernandodemorais@Luizs-MacBook-Pro.local>
2023-03-03 21:04:30 -0300

    chore: add another console log to test api keys errors

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-console.log('aqui info', process.env.STRIPE_SECRET_KEY!)
+console.log('aqui info', process.env.STRIPE_SECRET_KEY, process.env.NEXT_URL, process.env)
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: '2022-11-15',
